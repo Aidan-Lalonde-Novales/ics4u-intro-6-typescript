0e5570150f191c440df18e7b853fa09f35b8bb7f
Aidan-Lalonde-Novales <aidan.lalonde.novales@mths.ca>
2022-09-14 13:24:50 +0000

    i suck

---
+++ src/index.ts
@@ -0,0 +1,11 @@
+/**
+ * The program prints Hello World
+ *
+ * By:      Aidan Lalonde-Novales
+ * Version: 1.0
+ * Since:   2022-09-14
+ */
+
+console.log('Hello, World!')
+
+console.log('\nDone.')
